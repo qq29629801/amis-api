640e72fc9da838c1ecbf9181836b012a2c4a944a
bzGhost <296629801@qq.com>
2023-09-11 20:35:39 +0800

    部门

---
+++ apps/base/src/main/java/com/yatop/lambda/base/model/Dept.java
@@ -0,0 +1,48 @@
+package com.yatop.lambda.base.model;
+
+import com.yuyaogc.lowcode.engine.annotation.Id;
+import com.yuyaogc.lowcode.engine.annotation.Table;
+import com.yuyaogc.lowcode.engine.plugin.activerecord.Model;
+@Table(name = "base_dept")
+public class Dept extends Model<Dept> {
+
+    @Id
+    private Long id;
+
+    private String deptName;
+
+    /**
+     * 显示顺序
+     */
+    private Integer orderNum;
+
+    /**
+     * 负责人
+     */
+    private String leader;
+
+    /**
+     * 联系电话
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 部门状态:0正常,1停用
+     */
+    private String status;
+
+    /**
+     * 删除标志（0代表存在 2代表删除）
+     */
+    private String delFlag;
+
+    /**
+     * 祖级列表
+     */
+    private String ancestors;
+}
