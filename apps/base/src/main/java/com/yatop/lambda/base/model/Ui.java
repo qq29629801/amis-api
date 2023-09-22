06620aa117be1a4abf72a19d9ddaa5a752e3a66a
bzGhost <296629801@qq.com>
2023-09-22 21:12:10 +0800

    fix : ui

---
+++ apps/base/src/main/java/com/yatop/lambda/base/model/Ui.java
@@ -0,0 +1,33 @@
+package com.yatop.lambda.base.model;
+
+import com.yuyaogc.lowcode.engine.annotation.Table;
+import com.yuyaogc.lowcode.engine.plugin.activerecord.Model;
+
+import java.util.Date;
+
+@Table(name = "base_ui")
+public class Ui extends Model<Ui> {
+    private String app;
+    private String model;
+    private String type;
+    /**
+     * 创建人
+     */
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新人
+     */
+    private String updateBy;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
