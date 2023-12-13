425e507424fc7dc4a505117cd5e34c9e9211efc3
mjy <296629801@qq.com>
2023-12-13 10:56:24 +0800

    1

--- apps/net/src/main/java/com/yatop/lambda/net/websocket/hazelcast/HazelcastGroups.java
+++ apps/net/src/main/java/com/yatop/lambda/net/websocket/hazelcast/HazelcastGroups.java
@@ -1,4 +1,6 @@
 package com.yatop.lambda.net.websocket.hazelcast;
 
 public class HazelcastGroups {
+    public static final String Group_ID = "Group_ID_";
+
 }
--- apps/net/src/main/java/com/yatop/lambda/net/websocket/hazelcast/HazelcastMq.java
+++ apps/net/src/main/java/com/yatop/lambda/net/websocket/hazelcast/HazelcastMq.java
@@ -1,6 +1,35 @@
 package com.yatop.lambda.net.websocket.hazelcast;
 
+import com.yatop.lambda.net.websocket.protocol.Packet;
+
 public class HazelcastMq {
-    
+
+    public static final String userid_topc = "userId_topc";
+    public static final String group_topc = "group_topc";
+
+
+    private void receviceUser() {
+
+    }
+
+    private void receviceGroup() {
+
+    }
+
+    public void send2B(String toUserId, Packet packet) {
+
+        MessageBean messageBean = new MessageBean();
+        messageBean.setPacket(packet);
+        messageBean.setToUserId(toUserId);
+
+    }
+
+
+    public void sendToGroup(String groupId, String userId, Packet packet) {
+        MessageBean messageBean = new MessageBean();
+        messageBean.setPacket(packet);
+        messageBean.setUserId(userId);
+        messageBean.setGroupId(groupId);
+    }
 
 }
