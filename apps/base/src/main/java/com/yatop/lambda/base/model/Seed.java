f64fc5a547087336560cbf21c045d3acdc80d7f6
bzGhost <296629801@qq.com>
2024-06-22 16:05:58 +0800

    menus

--- apps/base/src/main/java/com/yatop/lambda/base/model/Seed.java
+++ apps/base/src/main/java/com/yatop/lambda/base/model/Seed.java
@@ -1,14 +1,9 @@
 package com.yatop.lambda.base.model;
 
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.crypto.digest.BCrypt;
 import com.yuyaogc.lowcode.engine.annotation.Service;
 import com.yuyaogc.lowcode.engine.annotation.Table;
 import com.yuyaogc.lowcode.engine.plugin.activerecord.Model;
-import com.yuyaogc.lowcode.engine.wrapper.LambdaQueryWrapper;
-import com.yuyaogc.lowcode.engine.wrapper.Wrappers;
 
-import java.util.Objects;
 
 @Table(name = "base_data_seed", isAbstract = true)
 public class Seed extends Model<Seed> {
@@ -16,37 +11,5 @@ public class Seed extends Model<Seed> {
 
     @Service(event = true)
     public void startUp() {
-        User user = new User();
-        Role role = new Role();
-
-
-        LambdaQueryWrapper<User> wrapper = Wrappers.lambdaQuery();
-        wrapper.eq(User::getUserName, "admin");
-        wrapper.eq(User::getLoginType, "sys_user");
-        User user1 = user.selectOne(wrapper);
-
-        if (Objects.isNull(user1)) {
-            user.setUserName("admin");
-            user.setPassword(BCrypt.hashpw("admin"));
-            user.setNickName(IdUtil.fastSimpleUUID());
-            user.setLoginType("sys_user");
-            user.save();
-        }
-
-
-        Role roleList = role.selectOne(new LambdaQueryWrapper<Role>().eq(Role::getRoleKey,"admin"));
-        if (Objects.isNull(roleList)) {
-            role.setRoleName("管理员");
-            role.setRoleKey("admin");
-            role.save();
-        }
-
-
-        if (Objects.nonNull(user.getId()) && Objects.nonNull(role.getId())) {
-            RoleUser roleUser = new RoleUser();
-            roleUser.set("userId", user.getId());
-            roleUser.set("roleId", role.getId());
-            roleUser.save();
-        }
     }
 }
