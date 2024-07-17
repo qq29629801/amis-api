86206ecf7f45a6b6d2d8986376915ed5c04ba88e
bzGhost <296629801@qq.com>
2024-07-17 22:31:39 +0800

    login

---
+++ service/src/main/java/com/yatop/lambda/api/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.yatop.lambda.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, HttpServletResponse response, ModelMap mmap)
+    {
+        return "login";
+    }
+}
